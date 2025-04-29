e216d0d8cd1eb82d027c557892b9f2189304e3d6
Jake <101604552+j4ke-exe@users.noreply.github.com>
2025-04-28 19:33:01 -0700

    Update coord.h

--- BootDev/C/Using-Typedef/coord.h
+++ BootDev/C/Using-Typedef/coord.h
@@ -1,6 +1,7 @@
 #pragma once
 
-typedef struct Coordinate { // Initiliaze typedef coordinate_t (_t is required)
+// Initialize typedef coordinate_t (_t is required)
+typedef struct Coordinate { // Order from largest to smallest to help the compiler minimize padding
   int x;
   int y;
   int z;
