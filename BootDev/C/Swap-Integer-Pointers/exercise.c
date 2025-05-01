979ff1eb85198071db2dcde93864fe24275ec3b7
Jake <101604552+j4ke-exe@users.noreply.github.com>
2025-05-01 15:10:52 -0700

    Update exercise.c

--- BootDev/C/Swap-Integer-Pointers/exercise.c
+++ BootDev/C/Swap-Integer-Pointers/exercise.c
@@ -1,5 +1,5 @@
 void swap_ints(int *a, int *b) {
-  int c = *a;
-  *a = *b;
-  *b = c;
+  int c = *a; // c becomes *a (as *c)
+  *a = *b; // *a becomes *b
+  *b = c; // *b becomes *a, stored in *c
 }
