bcbd115a081ea3eb1a1652fe65d71041fc4a928e
Jake <101604552+j4ke-exe@users.noreply.github.com>
2025-05-01 19:26:25 -0700

    Update permutation.py

--- HackerRank/Challenges/Python/permutation.py
+++ HackerRank/Challenges/Python/permutation.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
-    permutations = [ [i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n ]
-    print(permutations)
+    permutation = [ [i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n ]
+    print(permutation)
