cb2e34fd054b90b043837e1741f1c2c50cc18709
Jake <101604552+j4ke-exe@users.noreply.github.com>
2025-06-16 20:56:16 -0700

    Update objects.h

--- BootDev/C/Objects/objects.h
+++ BootDev/C/Objects/objects.h
@@ -4,41 +4,47 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-// Object types
-typedef enum {
-    INTEGER,
-    FLOAT,
-    STRING,
-    VECTOR3,
-    ARRAY
+// Object Types
+typedef enum
+{
+   INTEGER,
+   FLOAT,
+   STRING,
+   VECTOR3,
+   ARRAY
 } object_type_t;
 
-// Forward declaration
+// Forward Declaration
 typedef struct Object object_t;
 
-// 3D vector
-typedef struct {
-    object_t *x;
-    object_t *y;
-    object_t *z;
+// 3D Vector
+typedef struct
+{
+   object_t *x;
+   object_t *y;
+   object_t *z;
 } vector3_t;
 
-// Dynamic array
-typedef struct {
-    size_t size;
-    object_t **items;
+// Dynamic Array
+typedef struct
+{
+   size_t size;
+   object_t **items;
 } array_t;
 
-// Object structure
-struct Object {
-    object_type_t type;
-    union {
-        int v_int;
-        float v_float;
-        char *v_string;
-        vector3_t v_vector3;
-        array_t v_array;
-    } data;
+// Object Structure
+struct Object
+{
+   object_type_t type;
+   int refcount;
+   union
+   {
+      int v_int;
+      float v_float;
+      char *v_string;
+      vector3_t v_vector3;
+      array_t v_array;
+   } data;
 };
 
 // Constructors
@@ -48,7 +54,12 @@ object_t *new_string(char *value);
 object_t *new_vector3(object_t *x, object_t *y, object_t *z);
 object_t *new_array(size_t size);
 
-// Array Access
+// Reference Counting Functions
+void refcount_inc(object_t *obj);
+void refcount_dec(object_t *obj);
+void refcount_free(object_t *obj);
+
+// Array Access Functions
 object_t *array_get(object_t *obj, size_t index);
 bool array_set(object_t *obj, size_t index, object_t *value);
 
