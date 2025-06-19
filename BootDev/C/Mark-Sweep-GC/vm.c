bf1e2f55d5ce526314309d241b2a09e573787b92
Jake <101604552+j4ke-exe@users.noreply.github.com>
2025-06-19 13:53:29 -0700

    Update vm.c

--- BootDev/C/Mark-Sweep-GC/vm.c
+++ BootDev/C/Mark-Sweep-GC/vm.c
@@ -1,7 +1,25 @@
 #include "vm.h"
 #include "object.h"
 #include "stack.h"
-#include <stdlib.h>
+
+void vm_collect_garbage(vm_t *vm) {
+  mark(vm);
+  trace(vm);
+  sweep(vm);
+}
+
+void sweep(vm_t *vm) {
+  for (size_t i = 0; i < vm->objects->count; i++) {
+    object_t *obj = (object_t *)vm->objects->data[i];
+    if (obj->is_marked == true) {
+      obj->is_marked = false;
+    } else {
+      object_free(obj);
+      vm->objects->data[i] = NULL;
+    }
+  }
+  stack_remove_nulls(vm->objects);
+}
 
 void mark(vm_t *vm) {
   for (size_t i = 0; i < vm->frames->count; i++) {
@@ -13,11 +31,64 @@ void mark(vm_t *vm) {
   }
 }
 
+void trace(vm_t *vm) {
+  stack_t *gray_objects = stack_new(8);
+  if (gray_objects == NULL) {
+    return;
+  }
+
+  for (int i = 0; i < vm->objects->count; i++) {
+    object_t *obj = vm->objects->data[i];
+    if (obj->is_marked) {
+      stack_push(gray_objects, obj);
+    }
+  }
+
+  while (gray_objects->count > 0) {
+    trace_blacken_object(gray_objects, stack_pop(gray_objects));
+  }
+
+  stack_free(gray_objects);
+}
+
+void trace_blacken_object(stack_t *gray_objects, object_t *ref) {
+  object_t *obj = ref;
+
+  switch (obj->kind) {
+    case INTEGER:
+    case FLOAT:
+    case STRING:
+      break;
+    case VECTOR3: {
+      vector_t vec = obj->data.v_vector3;
+      trace_mark_object(gray_objects, vec.x);
+      trace_mark_object(gray_objects, vec.y);
+      trace_mark_object(gray_objects, vec.z);
+      break;
+    }
+    case ARRAY: {
+      for (size_t i = 0; i < obj->data.v_array.size; i++) {
+        trace_mark_object(gray_objects, obj->data.v_array.elements[i]);
+      }
+      break;
+    }
+  }
+}
+
+void trace_mark_object(stack_t *gray_objects, object_t *obj) {
+  if (obj == NULL || obj->is_marked) {
+    return;
+  }
+
+  stack_push(gray_objects, obj);
+  obj->is_marked = true;
+}
+
 void frame_reference_object(frame_t *frame, object_t *obj) {
   stack_push(frame->references, obj);
 }
 
-vm_t *vm_new(void) {
+vm_t *vm_new() {
   vm_t *vm = malloc(sizeof(vm_t));
   if (vm == NULL) {
     return NULL;
@@ -29,16 +100,12 @@ vm_t *vm_new(void) {
 }
 
 void vm_free(vm_t *vm) {
-  if (vm == NULL) {
-    return;
-  }
-
-  for (size_t i = 0; i < vm->frames->count; i++) {
+  for (int i = 0; i < vm->frames->count; i++) {
     frame_free(vm->frames->data[i]);
   }
   stack_free(vm->frames);
 
-  for (size_t i = 0; i < vm->objects->count; i++) {
+  for (int i = 0; i < vm->objects->count; i++) {
     object_free(vm->objects->data[i]);
   }
   stack_free(vm->objects);
@@ -48,20 +115,17 @@ void vm_free(vm_t *vm) {
 
 void vm_frame_push(vm_t *vm, frame_t *frame) { stack_push(vm->frames, frame); }
 
+frame_t *vm_frame_pop(vm_t *vm) { return stack_pop(vm->frames); }
+
 frame_t *vm_new_frame(vm_t *vm) {
   frame_t *frame = malloc(sizeof(frame_t));
-  if (frame == NULL) {
-    return NULL;
-  }
   frame->references = stack_new(8);
+
   vm_frame_push(vm, frame);
   return frame;
 }
 
 void frame_free(frame_t *frame) {
-  if (frame == NULL) {
-    return;
-  }
   stack_free(frame->references);
   free(frame);
 }
